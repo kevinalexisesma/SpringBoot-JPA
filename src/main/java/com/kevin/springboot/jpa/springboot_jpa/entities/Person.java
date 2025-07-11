2e482be5a478b061da2a80b5939a20e312d2b059
Kevin Eslava <kevinalexisesma@ufps.edu.co>
2025-07-11 11:14:58 -0500

    Feat(CicloVida): Eventos Ciclo de vida

--- src/main/java/com/kevin/springboot/jpa/springboot_jpa/entities/Person.java
+++ src/main/java/com/kevin/springboot/jpa/springboot_jpa/entities/Person.java
@@ -1,10 +1,14 @@
 package com.kevin.springboot.jpa.springboot_jpa.entities;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +25,12 @@ public class Person {
     @Column(name = "programming_language")
     private String programmingLanguage;
 
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
     public Person() {
     }
 
@@ -34,6 +44,18 @@ public class Person {
         this.name = name;
         this.lastName = lastName;
         this.programmingLanguage = programmingLanguage;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        System.out.println("Evento del ciclo de vida del entity pre persist");
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        System.out.println("Evento del ciclo de vida del entity pre update");
+        this.updatedAt = LocalDateTime.now();
     }
 
     public Long getId() {
@@ -71,7 +93,7 @@ public class Person {
     @Override
     public String toString() {
         return "[id=" + id + ", name=" + name + ", lastName=" + lastName + ", programmingLanguage="
-                + programmingLanguage + "]";
+                + programmingLanguage + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
     }
 
 }
